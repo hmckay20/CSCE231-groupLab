b742222db40b1e3903f214ada8f4059a794b5355
[Teighan Peterson} <89430237+teighannn@users.noreply.github.com>
2023-12-05 19:55:52 -0600

    Update alarm.c

--- groupProject/alarm.c
+++ groupProject/alarm.c
@@ -2,7 +2,7 @@
  *
  * @file alarm.c
  *
- * @author (STUDENTS -- TYPE YOUR NAMES HERE)
+ * @author Teighan Peterson, Erin Hughey, Hunter Mcay
  * @author (STUDENTS -- TYPE YOUR NAMES HERE)
  *
  * @brief Code to manage the piezodisc and LEDs.
@@ -25,11 +25,34 @@
 const unsigned int on_period = 7000;          // a made-up number, probably not the value you want to use
 volatile unsigned int total_period = 50000;
 
-
-void initialize_alarm(void) {
-  
+void initialize_alarm(void) 
+{
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    noInterrupts();
+    TCCR2A = 0;
+    TCCR2B = 0;
+    TCNT2 = 0;
+    OCR2A = 156; // (adjust as needed)
+    TCCR2A |= (1 << WGM21); // Set to ctc mode
+    TCCR2B |= (1 << CS22) | (1 << CS21); // Set prescaler to 256
+    TIMSK2 |= (1 << OCIE2A); // Enable compare match interrupt
+    interrupts();
 }
 
-void manage_alarm(void) {
-  
+void manage_alarm(void) 
+{
+    if (millis() % total_period < on_period) 
+    {
+        activate_alarm();
+    } 
+    else 
+    {
+        deactivate_alarm();
+    }
+}
+
+ISR(TIMER2_COMPA_vect)
+{
+
 }
